5246ae251ddb4383dbfc86d9270dcfb3bdb90051
maxg56 <mgendrot56@gmail.com>
2025-09-16 22:30:25 +0200

    refactor: Update author information in ft_strlen.c

--- src/ft_str/ft_strlen.c
+++ src/ft_str/ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgendrot <mgendrot@student.42.fr>          +#+  +:+       +#+        */
+/*   By: maxence <maxence@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 08:42:36 by mgendrot          #+#    #+#             */
-/*   Updated: 2024/10/18 17:45:12 by mgendrot         ###   ########.fr       */
+/*   Updated: 2025/09/16 22:30:13 by maxence          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,11 @@ size_t	ft_strlen(const char *str)
 		i++;
 	return (i);
 }
+
+// size_t	ft_strlen(const char *str)
+// {
+// 	if (!str || *str == '\0')
+// 		return (0);
+// 	return (1 + ft_strlen(str + 1));
+// }
+
