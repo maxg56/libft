cf17f6f02776b18d8a5c251f0ddc235b9237db7c
maxg56 <mgendrot56@gmail.com>
2025-09-17 17:45:43 +0200

    refactor: Update author information and correct function name in ft_lstclear.c

--- src/ft_lst/ft_lstclear.c
+++ src/ft_lst/ft_lstclear.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstclear_bons.c                                 :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgendrot <mgendrot@student.42.fr>          +#+  +:+       +#+        */
+/*   By: maxence <maxence@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 18:00:11 by mgendrot          #+#    #+#             */
-/*   Updated: 2024/10/23 09:18:40 by mgendrot         ###   ########.fr       */
+/*   Updated: 2025/09/17 17:44:37 by maxence          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,10 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*tmp;
 
-	if (lst)
+	while (lst && *lst)
 	{
-		while (*lst)
-		{
-			tmp = (*lst)->next;
-			ft_lstdelone(*lst, del);
-			(*lst) = tmp ;
-		}
+		tmp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		(*lst) = tmp ;
 	}
 }
